6b3efbbaa8a41b999d0d56ef599400394458efaf
Dmitry Petrov <166365976+pds05@users.noreply.github.com>
2025-08-07 17:16:26 +0400

    completed

---
+++ src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -0,0 +1,31 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+@Service
+@RequiredArgsConstructor
+public class TestRunnerServiceImpl implements TestRunnerService {
+
+    private final TestService testService;
+
+    private final StudentService studentService;
+
+    private final ResultService resultService;
+
+    private final LocalizedIOService localizedIOService;
+
+    @Override
+    public void run() {
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (QuestionReadException qre) {
+            localizedIOService.printFormattedLineLocalized("ResultService.error.test", "question error occurred");
+        } catch (Exception e) {
+            localizedIOService.printFormattedLineLocalized("ResultService.error.test", "common error occurred");
+        }
+    }
+}
